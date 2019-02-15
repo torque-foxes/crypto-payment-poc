cbe1a8502e9416eadbfde4c70d41adba0961fc12
shoosah <sshahbazi.nz@gmail.com>
2019-02-15 17:56:27 +1300

    Add more unit tests for FlyoutPanel component

--- themes/app/tests/FlyoutPanel.test.js
+++ themes/app/tests/FlyoutPanel.test.js
@@ -1,11 +1,40 @@
-import { shallowMount } from '@vue/test-utils'
-import FlyoutPanelComponent from '../src/js/components/FlyoutPanel'
+import { shallowMount } from '@vue/test-utils';
+import FlyoutPanelComponent from '../src/js/components/FlyoutPanel';
+
+const createCmp = propsData => shallowMount(FlyoutPanelComponent, { propsData })
 
 describe('FlyoutPanelComponent.test.js', () => {
-  let wrapper
+  let cmp
 
-  test('is a Vue instance', () => {
-    wrapper = shallowMount(FlyoutPanelComponent)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
+  beforeEach(() => {
+    cmp = createCmp({});
+  });
+
+  it('is a Vue instance', () => {
+    expect(cmp.isVueInstance()).toBeTruthy();
+  });
+
+  describe('Properties', () => {
+    it('returns the default value of `open` property', () => {
+      expect(cmp.props().open).toBe(false);
+    });
+
+    it('returns the default value of `slideFrom` property', () => {
+      expect(cmp.props().slideFrom).toBe('right');
+    });
+
+    describe('Validation', () => {
+      const slideFrom = createCmp({ slideFrom: 'right' }).vm.$options.props.slideFrom
+
+      it('validates `slideFrom` is of type string', () => {
+        expect(slideFrom.type).toBe(String);
+      });
+
+      it('validates the value of `slideFrom` property is to be within array [\'right\', \'left\']', () => {
+        expect(slideFrom.validator && slideFrom.validator('invalid-value')).toBeFalsy();
+        expect(slideFrom.validator && slideFrom.validator('right')).toBeTruthy();
+        expect(slideFrom.validator && slideFrom.validator('left')).toBeTruthy();
+      });
+    });
+  });
+});
