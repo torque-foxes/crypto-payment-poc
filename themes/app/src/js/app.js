79c56458cdade26a4504e2135d370c69e00ce07f
adunn <adunn@silverstripe.com>
2020-02-14 14:07:18 +1300

    #96 Since we're adding the Tabs component to default app config, also add TabItem so that sample Tabs boilerplate code can be used Also added promise polyfill to fix javacsript errors in IE11 with introduction of vuex in master branch.

--- themes/app/src/js/app.js
+++ themes/app/src/js/app.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Promise from 'promise-polyfill';
 import Meganav from './components/Meganav.vue';
 import FlyoutPanel from './components/FlyoutPanel/FlyoutPanel.vue';
 import Tabs from './components/Tabs/Tabs.vue';
+import Tab from './components/Tabs/TabItem.vue';
+
+// IE support
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 
 /**
  * Enable Vuex
@@ -34,6 +41,7 @@ new Vue({
   components: {
     FlyoutPanel,
     Tabs,
+    Tab,
   },
   mixins: [Meganav],
   data() {
