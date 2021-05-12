8aa68e00752e4232dfa715fb64bbfa042659c3eb
StephenMak <stephen.makrogianni@silverstripe.com>
2021-05-12 17:34:39 +1200

    Update custom embed shortcode parser
    - Fix typo in default height value
    - Update docblocks
    - Added linting rule to prevent the need to namespace FQ global functions

--- app/src/View/CustomEmbedShortcodeProvider.php
+++ app/src/View/CustomEmbedShortcodeProvider.php
@@ -16,11 +16,13 @@ class CustomEmbedShortcodeProvider extends EmbedShortcodeProvider
     /**
      * Define a default video height based on default page width dimensions
      */
-    private const DEFAULT_HEIGHT = 723;
+    private const DEFAULT_HEIGHT = 720;
 
     /**
      * Used to ensure videos are responsive
      *
+     * @param int $width The width of the video defined by the user or using the default value
+     * @param int $height The height of the video defined by the user or using the default value
      * @return float
      */
     public static function calculateRatioPadding(int $width, int $height): float
@@ -29,21 +31,24 @@ class CustomEmbedShortcodeProvider extends EmbedShortcodeProvider
     }
 
     /**
+     * @param array $arguments
+     * @param string $content
+     * @param ShortcodeParser $parser
+     * @param string $shortcode
+     * @param array $extra
      * @return string
+     * @phpcs:disable
      */
-    public static function handle_shortcode(
-        array $arguments,
-        string $content,
-        ShortcodeParser $parser,
-        string $shortcode,
-        array $extra = array()
-    ): string {
+    public static function handle_shortcode($arguments, $content, $parser, $shortcode, $extra = array()): string
+    {
         // Ensure we define a height and width
         $arguments['width'] ??= self::DEFAULT_WIDTH;
         $arguments['height'] ??= self::DEFAULT_HEIGHT;
-        $arguments['ratioPadding'] = self::calculateRatioPadding((int)$arguments['width'], (int)$arguments['height']);
+        $arguments['ratioPadding'] = self::calculateRatioPadding(
+            (int)$arguments['width'],
+            (int)$arguments['height']
+        );
 
         return parent::handle_shortcode($arguments, $content, $parser, $shortcode, $extra);
     }
-
 }
