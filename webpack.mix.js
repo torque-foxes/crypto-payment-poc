f120ebf4a29d5054274217bbb5179bd91df6a776
Dylan Wagstaff <NightJar@users.noreply.github.com>
2021-06-29 00:02:14 +1200

    Update windows yarn build copy dist files check
    Windows does support a feature similar to symbolic links, and most modern systems are smart enough to know to use it when a symlink is asked for. This makes the "do a copy on Windows" no longer a default true, and we must check if we need to copy or not. In some cases a copy may still be needed, so simply removing the existing check is insufficient - an edge case but a case none the less.

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const mix = require("laravel-mix");
 const webpack = require("webpack");
+const fs = require("fs");
 
 const srcFolder = `./themes/app/src`;
 const distFolder = `./themes/app/dist`;
@@ -72,11 +73,24 @@ if (process.env.NODE_ENV === "development") {
 
 /**
  * Windows specific config
- * Because windows can't handle symlinks we need to copy the
- * transpiled dependencies to the public directory.
+ *
+ * Depending on the version and setup of Windows, it will either not support symlinks entirely
+ * or use a junction folder in place (a Windows feature comparible to Unix's symbolic links).
+ *
+ * The former results in need of copying data, the latter works out of the box.
+ * We need to check the file type (directory, file, symlink, etc.) to know whether or not to copy.
+ *
+ * However, a junction presents itself to NodeJS as a standard directory, not as a symlink, and
+ * and in turn causes "Error: Source and destination must not be the same." on attempted copy.
+ * Luckily `realpath` returns as expected, so we can get around this by checking if source and
+ * destination are the same.
+ *
  * NOTE: This needs to be run after all other transpile actions.
  */
-if (process.platform === "win32") {
-  const publicResourcesFolder = `public/_resources/${distFolder}`;
+const publicResourcesFolder = `public/_resources/${distFolder}`;
+if (
+  process.platform === "win32"
+  && fs.realpathSync(distFolder) !== fs.realpathSync(publicResourcesFolder)
+) {
   mix.copyDirectory(distFolder, publicResourcesFolder);
 }
