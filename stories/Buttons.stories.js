ab4e28177daa11c9a4d66dbab784dca1472b50d5
Ed Wilde <ed.wilde@silverstripe.com>
2021-10-05 15:41:39 +1300

    Use a single button story

--- stories/Buttons.stories.js
+++ stories/Buttons.stories.js
@@ -4,21 +4,21 @@ storiesOf("Styleguide", module).add("Buttons", () => ({
   template: `
       <div>
         <button type="button" class="btn btn-primary">Primary</button>
-        <br>
+        <br><br>
         <button type="button" class="btn btn-secondary">Secondary</button>
-        <br>
+        <br><br>
         <button type="button" class="btn btn-success">Success</button>
-        <br>
+        <br><br>
         <button type="button" class="btn btn-danger">Danger</button>
-        <br>
+        <br><br>
         <button type="button" class="btn btn-warning">Warning</button>
-        <br>
+        <br><br>
         <button type="button" class="btn btn-info">Info</button>
-        <br>
+        <br><br>
         <button type="button" class="btn btn-light">Light</button>
-        <br>
+        <br><br>
         <button type="button" class="btn btn-dark">Dark</button>
-        <br>
+        <br><br>
         <button type="button" class="btn btn-link">Link</button>
       </div>
     `,
