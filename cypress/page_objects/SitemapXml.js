ab5dd6494af7d8a6019fa9c5e747acfaa061de9a
Ed Wilde <ed.wilde@silverstripe.com>
2022-01-07 11:26:04 +1300

    Use a real page in the tests
    So they don't fail

--- cypress/page_objects/SitemapXml.js
+++ cypress/page_objects/SitemapXml.js
@@ -7,7 +7,7 @@ module.exports = {
   sitemap: 'sitemap.xml/sitemap/SilverStripe-CMS-Model-SiteTree/1',
   // choose a single page uri to check for in the sitemap xml
   // all published pages are include by default
-  include: '/example-page-to-include-in-sitemap/',
+  include: '/about-us/',
   // choose a single page uri to make sure is *not* in the sitemap xml
   // this is defined by `ShowInSearch` being set to false
   exclude: '/example-page-to-exclude-from-sitemap/',
