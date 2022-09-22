7db8cd0264f1ef5f165d7dd19b142093e3a4f483
y2ksoft@gmail.com <y2ksoft@gmail.com>
2022-09-22 14:35:23 +1200

    CPM-4: added react components
    added React components to project
    created seperate folders in theme/app folder
    removed the Vue js setting from app.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,29 +7,17 @@ const webpack = require("webpack");
 const fs = require("fs");
 const globImporter = require('node-sass-glob-importer');
 
-// parse environment variables from .env
-require("dotenv").config();
-
 // define paths
 const srcFolder = `./themes/app/src`;
 const distFolder = `./themes/app/dist`;
 const publicFolder = `/_resources/${distFolder.substring(2)}/`; // remove leading ./
 
-const sassOptions = {
-  sassOptions: {
-    // Allow SCSS import wildcards
-    importer: globImporter(),
-    // Include cache-buster on urls
-    processCssUrls: true,
-  }
-};
 
 // Do the mix!
 mix
   .js(`${srcFolder}/js/app.js`, "/")
-  .vue({ version: 3 })
-  .sass(`${srcFolder}/scss/app.scss`, "/", sassOptions)
-  .sass(`${srcFolder}/scss/editor.scss`, "/", sassOptions);
+  .sass(`${srcFolder}/scss/app.scss`, "/")
+  .sass(`${srcFolder}/scss/editor.scss`, "/");
 
 // Places images processed in scss into themes/app/dist folder
 mix.copyDirectory(`${srcFolder}/images`, `${distFolder}/images`);
@@ -37,65 +25,55 @@ mix.copyDirectory(`${srcFolder}/icons`, `${distFolder}/icons`);
 mix.setPublicPath(distFolder);
 mix.setResourceRoot(publicFolder); // Prefixes urls in processed css with _resources/themes/app/dist
 
-/**
- * Setup vue correctly
- */
-mix.webpackConfig({
-  plugins: [
-    new webpack.DefinePlugin({
-      __VUE_OPTIONS_API__: true,
-      __VUE_PROD_DEVTOOLS__: false,
-    }),
-  ],
-});
+
 
 
 
 /**
  * Development specific
  */
-if (process.env.NODE_ENV === "development") {
-  // Add style lint
-  // eslint-disable-next-line global-require
-  const StyleLintPlugin = require("stylelint-webpack-plugin");
-  mix.webpackConfig({
-    plugins: [
-      new StyleLintPlugin({
-        context: srcFolder,
-        files: ["**/*.{scss,vue}"],
-      }),
-    ],
-  });
-
-  // Add eslint
-  mix.webpackConfig({
-    module: {
-      rules: [
-        {
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          exclude: /node_modules/,
-          loader: "eslint-loader",
-        },
-      ],
-    },
-  });
-
-  // This allows you to proxy your site while watching, meaning when you change
-  // your css/scss the file will get injected rather than requiring a reload
-  if (process.env.MIX_BROWSERSYNC === 'true') {
-    mix.browserSync({
-      proxy: process.env.HOSTNAME,
-      files: [`${distFolder}/**.*`],
-    });
-  }
-
-  // Add sourcemaps in depending on the scenario you might want to
-  // use these in prod too if the unminified code is fine to share
-  // as it can make solving bugs easier
-  mix.sourceMaps();
-  mix.webpackConfig({ devtool: "inline-source-map" });
-}
+// if (process.env.NODE_ENV === "development") {
+//   // Add style lint
+//   // eslint-disable-next-line global-require
+//   const StyleLintPlugin = require("stylelint-webpack-plugin");
+//   mix.webpackConfig({
+//     plugins: [
+//       new StyleLintPlugin({
+//         context: srcFolder,
+//         files: ["**/*.{scss,vue}"],
+//       }),
+//     ],
+//   });
+//
+//   // Add eslint
+//   mix.webpackConfig({
+//     module: {
+//       rules: [
+//         {
+//           enforce: "pre",
+//           test: /\.(js|vue)$/,
+//           exclude: /node_modules/,
+//           loader: "eslint-loader",
+//         },
+//       ],
+//     },
+//   });
+//
+//   // This allows you to proxy your site while watching, meaning when you change
+//   // your css/scss the file will get injected rather than requiring a reload
+//   if (process.env.MIX_BROWSERSYNC === 'true') {
+//     mix.browserSync({
+//       proxy: process.env.HOSTNAME,
+//       files: [`${distFolder}/**.*`],
+//     });
+//   }
+//
+//   // Add sourcemaps in depending on the scenario you might want to
+//   // use these in prod too if the unminified code is fine to share
+//   // as it can make solving bugs easier
+//   mix.sourceMaps();
+//   mix.webpackConfig({ devtool: "inline-source-map" });
+// }
 
 /**
  * Windows specific config
